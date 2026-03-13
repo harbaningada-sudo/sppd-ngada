54c992fc76e09a9a38cf811f5ac4f9a16e654914
harbaningada-sudo <harbaningada@gmail.com>
2026-03-13 11:19:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,95 +25,106 @@ with st.sidebar:
 
 # FORMAT TANGGAL
 bulan_list = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
-tgl_indo = f"{tgl_tetap.day} {bulan_list[tgl_tetap.month-1]} {tgl_tetap.year}"
+tgl_indo = "{0} {1} {2}".format(tgl_tetap.day, bulan_list[tgl_tetap.month-1], tgl_tetap.year)
 
 # 3. RENDER TAMPILAN
-# Bagian ini menggabungkan CSS dan HTML langsung agar tidak error
-st.markdown(
-    f"""
-    <style>
-        header, footer, #MainMenu {{visibility: hidden;}}
-        .stDeployButton {{display:none;}}
-        .main {{ background-color: #525659; }}
-        .kertas-a4 {{
-            background-color: white;
-            width: 210mm;
-            min-height: 297mm;
-            padding: 15mm 20mm 20mm 25mm;
-            margin: 20px auto;
-            color: black;
-            font-family: "Arial", sans-serif;
-            font-size: 11pt;
-            box-shadow: 0 0 15px rgba(0,0,0,0.5);
-            box-sizing: border-box;
-        }}
-        .kop-header {{ 
-            text-align: center; 
-            border-bottom: 3px solid black; 
-            padding-bottom: 2px; 
-            margin-bottom: 15px;
-            line-height: 1.1;
-        }}
-        .tabel-data {{ width: 100%; border-collapse: collapse; margin-bottom: 10px; }}
-        .tabel-data td {{ border: none !important; padding: 2px 0; vertical-align: top; }}
-        .text-center {{ text-align: center; }}
-        .text-bold {{ font-weight: bold; }}
-        .text-underline {{ text-decoration: underline; }}
-        .text-justify {{ text-align: justify; }}
-        @media print {{
-            .stSidebar, .stButton {{ display: none !important; }}
-            .main {{ background-color: white !important; }}
-            .kertas-a4 {{ box-shadow: none !important; margin: 0 !important; }}
-        }}
-    </style>
+# Saya buat template HTML murni tanpa f-string di dalamnya agar tidak error
+html_template = """
+<style>
+    header, footer, #MainMenu {{visibility: hidden;}}
+    .stDeployButton {{display:none;}}
+    .main {{ background-color: #525659; }}
+    .kertas-a4 {{
+        background-color: white;
+        width: 210mm;
+        min-height: 297mm;
+        padding: 15mm 20mm 20mm 25mm;
+        margin: 20px auto;
+        color: black;
+        font-family: "Arial", sans-serif;
+        font-size: 11pt;
+        box-shadow: 0 0 15px rgba(0,0,0,0.5);
+        box-sizing: border-box;
+    }}
+    .kop-header {{ 
+        text-align: center; 
+        border-bottom: 3px solid black; 
+        padding-bottom: 2px; 
+        margin-bottom: 15px;
+        line-height: 1.1;
+    }}
+    .tabel-data {{ width: 100%; border-collapse: collapse; margin-bottom: 10px; }}
+    .tabel-data td {{ border: none !important; padding: 2px 0; vertical-align: top; }}
+    .text-center {{ text-align: center; }}
+    .text-bold {{ font-weight: bold; }}
+    .text-underline {{ text-decoration: underline; }}
+    .text-justify {{ text-align: justify; }}
+    @media print {{
+        .stSidebar, .stButton {{ display: none !important; }}
+        .main {{ background-color: white !important; }}
+        .kertas-a4 {{ box-shadow: none !important; margin: 0 !important; }}
+    }}
+</style>
 
-    <div class="kertas-a4">
-        <div class="kop-header">
-            <h3 class="text-bold" style="margin:0; font-size: 14pt;">PEMERINTAH KABUPATEN NGADA</h3>
-            <h3 class="text-bold" style="margin:0; font-size: 14pt;">SEKRETARIAT DAERAH</h3>
-            <p style="margin:0; font-size:10pt;">Jln. Soekarno - Hatta No. 1 Telp (0384) 21012</p>
-            <h3 class="text-bold" style="margin:0; font-size: 14pt;">BAJAWA</h3>
-        </div>
+<div class="kertas-a4">
+    <div class="kop-header">
+        <h3 class="text-bold" style="margin:0; font-size: 14pt;">PEMERINTAH KABUPATEN NGADA</h3>
+        <h3 class="text-bold" style="margin:0; font-size: 14pt;">SEKRETARIAT DAERAH</h3>
+        <p style="margin:0; font-size:10pt;">Jln. Soekarno - Hatta No. 1 Telp (0384) 21012</p>
+        <h3 class="text-bold" style="margin:0; font-size: 14pt;">BAJAWA</h3>
+    </div>
 
-        <h3 class="text-center text-bold text-underline" style="margin-top: 10px; margin-bottom:0;">SURAT PERINTAH TUGAS</h3>
-        <p class="text-center" style="margin-top:0;">NOMOR : {no_spt}</p>
+    <h3 class="text-center text-bold text-underline" style="margin-top: 10px; margin-bottom:0;">SURAT PERINTAH TUGAS</h3>
+    <p class="text-center" style="margin-top:0;">NOMOR : {no_spt}</p>
 
-        <br>
+    <br>
 
+    <table class="tabel-data">
+        <tr><td width="15%">Dasar</td><td width="2%">:</td><td class="text-justify">{dasar}</td></tr>
+    </table>
+
+    <p class="text-center" style="letter-spacing: 3px; margin: 15px 0;">M E M E R I N T A H K A N</p>
+
+    <table class="tabel-data">
+        <tr><td width="15%">Kepada</td><td width="5%">: 1.</td><td width="15%">Nama</td><td width="2%">:</td><td class="text-bold">{nama}</td></tr>
+        <tr><td></td><td></td><td>Pangkat/Gol</td><td>:</td><td>{gol}</td></tr>
+        <tr><td></td><td></td><td>NIP</td><td>:</td><td>{nip}</td></tr>
+        <tr><td></td><td></td><td>Jabatan</td><td>:</td><td>{jabatan}</td></tr>
+    </table>
+
+    <br>
+
+    <table class="tabel-data">
+        <tr><td width="15%">Untuk</td><td width="2%">:</td><td class="text-justify">{untuk}</td></tr>
+    </table>
+
+    <div style="margin-left:50%; margin-top:30px;">
         <table class="tabel-data">
-            <tr><td width="15%">Dasar</td><td width="2%">:</td><td class="text-justify">{dasar}</td></tr>
+            <tr><td width="45%">Ditetapkan di</td><td>:</td><td>Bajawa</td></tr>
+            <tr><td>Pada Tanggal</td><td>:</td><td>{tgl_indo}</td></tr>
         </table>
-
-        <p class="text-center" style="letter-spacing: 3px; margin: 15px 0;">M E M E R I N T A H K A N</p>
-
-        <table class="tabel-data">
-            <tr><td width="15%">Kepada</td><td width="5%">: 1.</td><td width="15%">Nama</td><td width="2%">:</td><td class="text-bold">{nama}</td></tr>
-            <tr><td></td><td></td><td>Pangkat/Gol</td><td>:</td><td>{gol}</td></tr>
-            <tr><td></td><td></td><td>NIP</td><td>:</td><td>{nip}</td></tr>
-            <tr><td></td><td></td><td>Jabatan</td><td>:</td><td>{jabatan}</td></tr>
-        </table>
-
-        <br>
-
-        <table class="tabel-data">
-            <tr><td width="15%">Untuk</td><td width="2%">:</td><td class="text-justify">{untuk}</td></tr>
-        </table>
-
-        <div style="margin-left:50%; margin-top:30px;">
-            <table class="tabel-data">
-                <tr><td width="45%">Ditetapkan di</td><td>:</td><td>Bajawa</td></tr>
-                <tr><td>Pada Tanggal</td><td>:</td><td>{tgl_indo}</td></tr>
-            </table>
-            <div style="margin: 15px 0 0 25px;">
-                <p class="text-bold" style="margin:0;">An. BUPATI NGADA</p>
-                <p style="margin:0;">Pj. Sekretaris Daerah,</p>
-                <br><br><br><br>
-                <p class="text-bold text-underline" style="margin:0;">{nama_sekda}</p>
-                <p style="margin:0;">{gol}</p>
-                <p style="margin:0;">NIP. {nip_sekda}</p>
-            </div>
+        <div style="margin: 15px 0 0 25px;">
+            <p class="text-bold" style="margin:0;">An. BUPATI NGADA</p>
+            <p style="margin:0;">Pj. Sekretaris Daerah,</p>
+            <br><br><br><br>
+            <p class="text-bold text-underline" style="margin:0;">{nama_sekda}</p>
+            <p style="margin:0;">{gol}</p>
+            <p style="margin:0;">NIP. {nip_sekda}</p>
         </div>
     </div>
-    """,
-    unsafe_allow_html=True
-)
+</div>
+"""
+
+# Proses memasukkan data ke template secara aman
+st.markdown(html_template.format(
+    no_spt=no_spt,
+    dasar=dasar,
+    nama=nama,
+    nip=nip,
+    gol=gol,
+    jabatan=jabatan,
+    untuk=untuk,
+    tgl_indo=tgl_indo,
+    nama_sekda=nama_sekda,
+    nip_sekda=nip_sekda
+), unsafe_allow_html=True)
