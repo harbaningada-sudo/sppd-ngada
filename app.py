1dea1d89d89748110d647fb1b9cae33c2e4c4400
harbaningada-sudo <harbaningada@gmail.com>
2026-03-25 12:24:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import datetime
-import logo 
+import logo  # Memanggil file logo.py di repository kamu
 
 # 1. KONFIGURASI HALAMAN
 st.set_page_config(page_title="Sistem SPD Ngada Pro", layout="wide")
@@ -54,18 +54,20 @@ st.markdown("""
     .judul-rapat { text-align: center; line-height: 1.0 !important; margin-top: 5px; }
     .judul-rapat h3, .judul-rapat p { margin: 0; line-height: 1.0 !important; }
 
-    /* ISI NARASI SPT (Line Spacing 1.5) */
-    .isi-surat-spt { line-height: 1.5 !important; margin-top: 10px; }
+    /* PERBAIKAN: ISI NARASI SPT (Line Spacing 1.0 sesuai instruksi) */
+    .isi-surat-spt { line-height: 1.0 !important; margin-top: 10px; }
+    .isi-surat-spt table { line-height: 1.0 !important; }
 
     /* TABEL SPD DEPAN (Line Spacing 1.0) */
     .tabel-border { width: 100%; border-collapse: collapse !important; border: 1pt solid black !important; table-layout: fixed; }
-    .tabel-border td { border: 1pt solid black !important; padding: 4px 8px !important; vertical-align: top; color: black !important; font-size: 10pt; line-height: 1.1 !important; }
+    .tabel-border td { border: 1pt solid black !important; padding: 4px 8px !important; vertical-align: top; color: black !important; font-size: 10pt; line-height: 1.0 !important; }
     
+    /* SEJAJARKAN NOMOR 1. sd 10. */
     .col-no { width: 35px !important; text-align: left !important; }
 
     /* TABEL VISUM (FULL LEGAL SPACE) */
     .visum-table { width: 100%; border: none !important; border-collapse: collapse; margin: 0 !important; }
-    .visum-table td { border: none !important; padding: 0 !important; font-size: 10pt; line-height: 1.2; color: black !important; vertical-align: top; }
+    .visum-table td { border: none !important; padding: 0 !important; font-size: 10pt; line-height: 1.0 !important; color: black !important; vertical-align: top; }
 
     .text-center { text-align: center; } .text-bold { font-weight: bold; } .underline { text-decoration: underline; }
 
@@ -150,7 +152,7 @@ with st.sidebar:
 kop_pemda = f'''<table class="kop-table"><tr><td width="15%"><img src="data:image/png;base64,{logo.PEMDA}" width="75"></td><td class="kop-teks"><h3>PEMERINTAH KABUPATEN NGADA</h3><h2>SEKRETARIAT DAERAH</h2><p>Jln. Soekarno - Hatta No. 1 Telp (0384) 2225834</p><p class="text-bold">BAJAWA</p></td><td width="15%"></td></tr></table>'''
 
 def get_ttd(space): 
-    return f'''<div style="margin-left:55%; margin-top:10px; line-height:1.2;"><b>An. BUPATI NGADA</b><br>{jab_ttd},<br>{f"Ub. {ub}," if ub else ""}<div style="height:{space}px;"></div><b><u>{pjb}</u></b><br>{gol_pjb}<br>NIP. {nip_ttd}</div>'''
+    return f'''<div style="margin-left:55%; margin-top:10px; line-height:1.0;"><b>An. BUPATI NGADA</b><br>{jab_ttd},<br>{f"Ub. {ub}," if ub else ""}<div style="height:{space}px;"></div><b><u>{pjb}</u></b><br>{gol_pjb}<br>NIP. {nip_ttd}</div>'''
 
 html_out = '<div class="main-container">'
 
@@ -183,7 +185,7 @@ if tab_menu == "Input & Cetak":
             </table>{get_ttd(75)}</div>'''
 
     if "SPD Belakang" in opsi_cetak:
-        ttd_bk_html = f'''<div style="text-align:center; line-height:1.2; font-size:10pt; margin-top:5px;">
+        ttd_bk_html = f'''<div style="text-align:center; line-height:1.0; font-size:10pt; margin-top:5px;">
             <b>An. BUPATI NGADA</b><br>{jab_ttd},<br>{f"Ub. {ub}," if ub else ""}<div style="height:65px;"></div>
             <b><u>{pjb}</u></b><br>{gol_pjb}<br>NIP. {nip_ttd}</div>'''
         
@@ -196,17 +198,17 @@ if tab_menu == "Input & Cetak":
             <tr style="height: 190px;"><td>{rv("II.", "Tiba di", tujuan, tgl_bkt)}</td><td style="padding:10px;">{rv("", "Berangkat dari", tujuan, tgl_kbl, False)}<table class="visum-table"><tr><td width="35%">Ke</td><td width="5%">:</td><td>Bajawa</td></tr></table></td></tr>
             <tr style="height: 190px;"><td>{rv("III.", "Tiba di", "", "")}</td><td style="padding:10px;">{rv("", "Berangkat dari", "", "", False)}</td></tr>
             <tr style="height: 190px;"><td>{rv("IV.", "Tiba di", "", "")}</td><td style="padding:10px;">{rv("", "Berangkat dari", "", "", False)}</td></tr>
-            <tr style="height: 220px;"><td>{rv("V.", "Tiba Kembali", "Bajawa", tgl_kbl)}</td><td style="padding:10px;"><p style="font-style:italic; font-size:9.2pt; line-height:1.2; margin-top:5px;">Telah diperiksa, dengan keterangan bahwa perjalanan tersebut atas perintahnya dan semata-mata untuk kepentingan jabatan</p>{ttd_bk_html}</td></tr>
+            <tr style="height: 220px;"><td>{rv("V.", "Tiba Kembali", "Bajawa", tgl_kbl)}</td><td style="padding:10px;"><p style="font-style:italic; font-size:9.2pt; line-height:1.0;">Telah diperiksa, dengan keterangan bahwa perjalanan tersebut atas perintahnya dan semata-mata untuk kepentingan jabatan</p>{ttd_bk_html}</td></tr>
         </table>
         <div style="border:1pt solid black; border-top:none; padding:8px; font-size:10.5pt;"><b>VI. Catatan Lain-lain</b></div>
-        <div style="border:1pt solid black; border-top:none; padding:8px; font-size:8.6pt; text-align:justify; color:black; line-height:1.2;">
+        <div style="border:1pt solid black; border-top:none; padding:8px; font-size:8.6pt; text-align:justify; color:black; line-height:1.0;">
             <b>VII. Perhatian :</b><br>
             Pejabat yang menerbitkan SPD, pegawai yang melakukan perjalanan dinas, para pejabat yang mengesahkan tanggal berangkat/tiba, serta Bendahara Pengeluaran bertanggung jawab berdasarkan peraturan-peraturan Keuangan Negara apabila negara menderita rugi akibat kesalahan, kelalaian dan kealpaannya.
         </div></div>'''
 
     if "Register" in opsi_cetak:
-        r_rows = "".join([f"<tr><td class='text-center'>{i+1}</td><td>{r['Nama']}</td><td>{r['No SPT']}</td><td>{r['No SPD']}</td><td>{r['Berangkat']}</td><td>{r['Pulang']}</td><td>{r['Lama']}</td><td>-</td></tr>" for i, r in enumerate(st.session_state.arsip_register)])
-        html_out += f'''<div class="kertas kertas-landscape register-page"><h3 class="text-center text-bold">REGISTER SURAT PERJALANAN DINAS</h3><br><table class="tabel-border" style="font-size:9pt; width:100%;"><thead><tr style="background:#eee;"><th>No</th><th>Nama Pegawai</th><th>Nomor SPT</th><th>Nomor SPD</th><th>Tgl Bkt</th><th>Tgl Kbl</th><th>Lamanya</th><th>Ket</th></tr></thead><tbody>{r_rows}</tbody></table></div>'''
+        r_rows = "".join([f"<tr><td class='text-center'>{i+1}</td><td>{r['Nama']}</td><td>{r['No SPT']}</td><td>{r['No SPD']}</td><td>{r['Berangkat']}</td><td>{r['Pulang']}</td><td>{r['Lamanya']}</td><td>-</td></tr>" for i, r in enumerate(st.session_state.arsip_register)])
+        html_out += f'''<div class="kertas kertas-landscape register-page"><h3 class="text-center text-bold">REGISTER SURAT PERJALANAN DINAS</h3><br><table class="tabel-border" style="font-size:9pt; width:100%;"><thead><tr style="background:#eee;"><th>No</th><th>Nama Pegawai</th><th>Nomor SPT</th><th>Nomor SPD</th><th>Tanggal Berangkat</th><th>Tanggal Pulang</th><th>Lamanya</th><th>Ket</th></tr></thead><tbody>{r_rows}</tbody></table></div>'''
 
 html_out += '</div>'
 st.markdown(html_out, unsafe_allow_html=True)
